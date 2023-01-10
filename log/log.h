d5f7b15185fb20250a0fedbc33cce3c0f105e59c
ciaowhen <1421003699@qq.com>
2023-01-10 20:55:55 +0800

    commit Log

---
+++ log/log.h
@@ -0,0 +1,20 @@
+//
+// Created by user on 2023/1/10.
+//
+
+#ifndef LOG_H
+#define LOG_H
+
+#include <mutex>
+#include <string>
+#include <thread>
+#include <sys/time.h>
+
+class Log
+{
+public:
+    void Init()
+};
+
+
+#endif LOG_H
